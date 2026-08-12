29f501f9508b4de99d808aa181a8db2ea47a6e10
LeoCu <JLCE33748003@soy.sena.edu.co>
2026-08-12 16:42:58 -0500

    Tarea de leonardo y santiago BASE 1.1 4:42 12/08/2026

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './styles/layout.css';
 import App from './App.jsx'
-import Base from './base.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Base />
+    <App />
   </StrictMode>,
 )
