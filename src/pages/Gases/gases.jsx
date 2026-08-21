11fead001bda4dd57c26cdcd32f0e189ed12b832
danielarojaslosada-sud <danielarojaslosada@gmail.com>
2026-08-21 16:49:04 -0500

    Tarea de Daniela Medicion de gases 5:00 21/08/2026

---
+++ src/pages/Gases/gases.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import { Boton, BotonLink } from "../../components/common/Button";
+import { PageWelcome, Searcher } from "../../components/common/welcome";
+
+function Gases() {
+    return (
+        <div>
+            <PageWelcome
+                titulo="MEDICIÓN DE GASES"
+                descripcion="Este es el formato con el registro de gases."
+            />
+            <Searcher placeholder="Buscar por código, fecha, hora, observaciones "/>
+            <br />
+            <table>
+                <thead>
+                    <tr>
+                        <th>Código</th>
+                        <th>Fecha</th>
+                        <th>Hora</th>
+                        <th>Observaciones</th>
+                        <th>Responsable</th>
+                        <th>Acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td>
+                            <div className="btn-container">
+                                <BotonLink
+                                    link="/Gases/gas_format"
+                                    clase="btn-azul"
+                                    icono="fa-solid fa-eye"
+                                    title="Ver detalle"
+                                />
+                                <Boton
+                                    clase="btn-2"
+                                    icono="fa-solid fa-trash"
+                                    title="Eliminar"
+                                />
+                            </div>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <br/>
+            <div className="btn-container">
+                <BotonLink
+                    link="/gases/compare_gases"
+                    clase="btn-azul"
+                    icono="fa-solid fa-chart-line"
+                    texto="Comparar mediciones"
+                />
+            </div>
+        </div>
+    );
+}
+
+export default Gases;
